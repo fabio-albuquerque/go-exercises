2db59941b101f574e67f46853a13f2652be41d26
Fabio Albuquerque <fabioalbuquerque@orbcom.local>
2018-10-29 12:00:22 +0000

    level 3

--- jedi_levels.go
+++ jedi_levels.go
@@ -18,15 +18,27 @@ import (
 // }
 
 // Level 2
-var x int
-var y string
-var z bool
+// var x int
+// var y string
+// var z bool
+
+// func main() {
+
+// 	fmt.Printf("%v\n%v\n%v\n", x, y, z)
+
+// 	fmt.Println(x)
+// 	fmt.Println(y)
+// 	fmt.Println(z)
+// }
+
+// Level 3
+var x = 42
+var y = "James Bond"
+var z = true
 
 func main() {
 
-	fmt.Printf("%v\n%v\n%v\n", x, y, z)
+	s := fmt.Sprintf("%v\n%v\n%v\n", x, y, z)
 
-	fmt.Println(x)
-	fmt.Println(y)
-	fmt.Println(z)
+	fmt.Println(s)
 }
