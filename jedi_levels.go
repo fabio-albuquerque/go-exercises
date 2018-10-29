e443b26aca52b878d89951f032b0ac4d8fe8b7a9
Fabio Albuquerque <fabioalbuquerque@orbcom.local>
2018-10-29 11:46:54 +0000

    jedi levels

---
+++ jedi_levels.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Level 1
+// func main() {
+// 	x := 42
+// 	y := "James Bond"
+// 	z := true
+
+// 	fmt.Printf("%v\n%v\n%v\n", x, y, z)
+
+// 	fmt.Println(x)
+// 	fmt.Println(y)
+// 	fmt.Println(z)
+// }
+
+// Level 2
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Printf("%v\n%v\n%v\n", x, y, z)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
