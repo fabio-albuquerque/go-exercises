ae19f8e8983d75bd5022005cbc29f4d8e78b36a6
Fabio Albuquerque <fabioalbuquerque@orbcom.local>
2018-10-30 15:52:07 +0000

    tests

--- Tests/test.go
+++ Tests/test.go
@@ -1,21 +1,27 @@
 package main
 
-import "fmt"
-
-const (
-	a = iota
-	b
-	c = "42"
-	d = iota
-	e = 19
-	f
+import (
+	"fmt"
 )
 
+// const (
+// 	a = iota
+// 	b
+// 	c = "42"
+// 	d = iota
+// 	e = 19
+// 	f
+// )
+
 func main() {
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Println(d)
-	fmt.Println(e)
-	fmt.Println(f)
+	// fmt.Println(a)
+	// fmt.Println(b)
+	// fmt.Println(c)
+	// fmt.Println(d)
+	// fmt.Println(e)
+	// fmt.Println(f)
+
+	for i := 0; i <= 100; i++ {
+		fmt.Println(i)
+	}
 }
