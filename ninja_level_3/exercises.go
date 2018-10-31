9a93cf001b68496afdf5f15b1cfd137b1481c1e2
Fabio Albuquerque <fabioalbuquerque@orbcom.local>
2018-10-31 11:30:03 +0000

    ninja level 3 - exercise 2

--- ninja_level_3/exercises.go
+++ ninja_level_3/exercises.go
@@ -6,8 +6,19 @@ import (
 
 // Exercise 1
 
+// func main() {
+// 	for i := 1; i <= 10000; i++ {
+// 		fmt.Println(i)
+// 	}
+// }
+
+// Exercise 2
+
 func main() {
-	for i := 1; i <= 10000; i++ {
+	for i := 1; i <= 26; i++ {
 		fmt.Println(i)
+		for j := 1; j <= 3; j++ {
+			fmt.Printf("\t%#U\n", i+64)
+		}
 	}
 }
